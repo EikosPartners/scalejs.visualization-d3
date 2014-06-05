64e63e6a84e8678af2565714c348b5a978f9e03c
nicholasantonov <nicholasantonov@gmail.com>
2014-06-05 14:49:08 -0400

    restart

---
+++ scalejs.visualization-d3/scalejs.visualization-d3/gesture-helper.js
@@ -0,0 +1,55 @@
+﻿/*global define*/
+/*jslint devel: true */
+/*jslint browser: true */
+define([
+    'scalejs!core',
+    'd3'
+
+], function (
+    core,
+    d3
+) {
+    var transform = {
+        left: 0,
+        top: 0,
+        rotate: 0,
+        scale: 1
+    };
+
+    // Clear the canvas's transform and animate from current to cleared state:
+    function resetTransformAnimation(canvas) {
+        // Reset target transform:
+        transform.left = 0;
+        transform.top = 0;
+        transform.rotate = 0;
+        transform.scale = 1;
+        canvas.select("group").transition().duration(1000)
+            .tween("canvasTween", function () {
+                // Create interpolations used for a nice slide around the parent:
+                var interpLeft = d3.interpolate(this.left, 0),
+                    interpTop = d3.interpolate(this.top, 0),
+                    interpAngle = d3.interpolate(this.angle, 0),
+                    interpScaleX = d3.interpolate(this.scaleX, 1),
+                    interpScaleY = d3.interpolate(this.scaleY, 1),
+                    el = this;
+                return function (t) {
+                    el.left = interpLeft(t);
+                    el.top = interpTop(t);
+                    el.angle = interpAngle(t);
+                    el.scaleX = interpScaleX(t);
+                    el.scaleY = interpScaleY(t);
+                };
+            });
+    }
+
+
+    //====
+    function getTransform() {
+        return transform;
+    }
+
+    return {
+        getTransform: getTransform,
+        resetTransformAnimation: resetTransformAnimation
+    };
+});
