dd29a2cb595ed8097131d8ab1561bb999e95955f
nicholasantonov <nicholasantonov@gmail.com>
2014-06-23 12:15:49 -0400

    Add template to jsonhelper file

--- scalejs.visualization-d3/scalejs.visualization-d3/json-helper.js
+++ scalejs.visualization-d3/scalejs.visualization-d3/json-helper.js
@@ -1,1 +1,15 @@
-﻿
+﻿/*global define*/
+/*jslint devel: true */
+/*jslint browser: true */
+define([
+    'knockout',
+    'd3',
+    'd3.colorbrewer'
+], function (
+    ko,
+    d3,
+    colorbrewer
+) {
+    "use strict";
+
+});
